8ffd5fb66755187e459923713e502c1a0c1b7e89
Jack Chou <jackchou@berkeley.edu>
2014-10-28 02:34:29 -0700

    Populated projects page.

---
+++ js/scripts.js
@@ -0,0 +1,11 @@
+$(function(){$("[data-toggle='tooltip']").tooltip();});
+
+var url = document.location.toString();
+if (url.match('#')) {
+    $('.nav-tabs a[href=#'+url.split('#')[1]+']').tab('show');
+}
+
+$('.nav-tabs a').on('shown', function (e) {
+    window.location.hash = e.target.hash;
+})
+
